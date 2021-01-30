f4dc7ef0a7b2bec44129c99020619ea4dcc6dae2
Michael Semle <michael.semle@b13.com>
2021-01-30 12:02:15 +0100

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF['frontendrendering'] = [
+    'title' => 'Frontendrendering',
+    'description' => '',
+    'category' => 'frontend',
+    'state' => 'stable',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Michael Semle, mikeproductions',
+    'author_email' => 'entwicklung@mikeproduction.de',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '<=10.9.99',
+        ],
+    ],
+];

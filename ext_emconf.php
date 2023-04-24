3d9b0e8caf9a2e57227e594bd75c55bde8f9c300
Michael Semle <git@mikeproduction.de>
2023-04-24 21:54:25 +0200

    [TASK] Add v12 support, drop v10 support, raise version and cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,16 +2,16 @@
 
 $EM_CONF['frontendrendering'] = [
     'title' => 'Frontendrendering',
-    'description' => '',
+    'description' => 'Provides basic typoscript configuration for templating',
     'category' => 'frontend',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Michael Semle, mikeproductions',
     'author_email' => 'entwicklung@mikeproduction.de',
-    'version' => '333.0.0',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.99.99',
+            'typo3' => '11.5.0-12.99.99',
         ],
     ],
 ];
